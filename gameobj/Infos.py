96b46b0ebad649fd220fb8db40a2c3920c20bbad
SergeMaikere <serge.maikere@gmail.com>
2026-03-06 19:31:33 +0100

    + display correct name and health on switch

--- gameobj/Infos.py
+++ gameobj/Infos.py
@@ -16,11 +16,7 @@ class Infos:
 		self.font = pygame.font.Font(None, 30)
 
 		self.info_rect = pygame.FRect(self.left -20, self.top -60, 250, 80)
-		self.name_surf = self.font.render(self.__get_monster().name, True, COLORS['black'])
-		self.name_rect = self.name_surf.get_frect(topleft=self.info_rect.topleft + pygame.Vector2(self.info_rect.width * 0.05, 12))
 
-		self.health_rect = pygame.FRect(self.name_rect.left, self.name_rect.bottom + 10, self.info_rect.width * 0.9, 20 )
-		self.ratio = self.health_rect.width / self.__get_monster().max_health
 
 	def __get_monster ( self ): return self.monster_manager.player_monster if self.subject == 'player' else self.monster_manager.opponent_monster
 
@@ -28,11 +24,17 @@ class Infos:
 		pygame.draw.rect(self.canvas, COLORS['white'], rect, 0, 4)
 		pygame.draw.rect(self.canvas, COLORS['gray'], rect, 4, 4)
 
-	def __draw_monster_name ( self ): self.canvas.blit(self.name_surf, self.name_rect)
+	def __draw_monster_name ( self ): 
+		self.name_surf = self.font.render(self.__get_monster().name, True, COLORS['black'])
+		self.name_rect = self.name_surf.get_frect(topleft=self.info_rect.topleft + pygame.Vector2(self.info_rect.width * 0.05, 12))
+		self.canvas.blit(self.name_surf, self.name_rect)
 
 	def __draw_health_bar ( self ):
-		progress_bar = pygame.FRect(self.health_rect.left, self.health_rect.top, self.__get_monster().health * self.ratio, self.health_rect.height)
-		pygame.draw.rect(self.canvas, COLORS['gray'], self.health_rect)
+		health_rect = pygame.FRect(self.name_rect.left, self.name_rect.bottom + 10, self.info_rect.width * 0.9, 20 )
+		ratio = health_rect.width / self.__get_monster().max_health
+		progress_bar = pygame.FRect(health_rect.left, health_rect.top, self.__get_monster().health * ratio, health_rect.height)
+		
+		pygame.draw.rect(self.canvas, COLORS['gray'], health_rect)
 		pygame.draw.rect(self.canvas, COLORS['red'], progress_bar)
 
 	def draw ( self ):
